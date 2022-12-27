7daf426aaafb2d8ed3780a62016667656431a994
utkarsh1996-3 <119508950+utkarsh1996-3@users.noreply.github.com>
2022-12-27 20:52:33 +0530

    solution

--- solution.js
+++ solution.js
@@ -1,6 +1,13 @@
-// complete the given function
+// c// complete the given function
 
 function palindrome(str){
+  str = str.toLowerCase();
+  str = str.replace(/[^a-z0-9]/g, '');
+  let backward = str.split('');
+  backward = backward.reverse().join('').toString();
+  return backward == str;
 
 }
+// console.log(palindrome("hello"));
+// console.log(palindrome("race car"));
 module.exports = palindrome
